6d00a90ee487fbd76f90eaf54395e111e3880d69
Hermas Francisco <franciscohermas@gmail.com>
2025-12-16 11:04:59 +0100

    Managing Users : Create UserController, BaseUserRequest, ReplaceUserRequest, UserPolicy; Update User model, UserResource, StoreUserRequest, UpdateUserRequest, AuthorsController, AppServiceProvider; Add new routes to API V1 Routes;

---
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Filters\V1\AuthorFilter;
+use App\Http\Requests\Api\V1\ReplaceUserRequest;
+use App\Http\Requests\Api\V1\StoreUserRequest;
+use App\Http\Requests\Api\V1\UpdateUserRequest;
+use App\Http\Resources\V1\UserResource;
+use App\Models\User;
+use App\Policies\V1\UserPolicy;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+class UserController extends ApiController
+{
+    protected $policyClass = UserPolicy::class;
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(AuthorFilter $filters)
+    {
+        return UserResource::collection(
+            User::filter($filters)->paginate()
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request)
+    {
+        try {
+            // Policy
+            $this->isAble('store', User::class);
+
+            // Create ticket
+            return new UserResource(User::create($request->mappedAttributes()));
+        } catch (AuthorizationException $ex) {
+            return $this->error('You are not authorized to create that resource.', 401);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        if ($this->include('tickets')) {
+            return new UserResource($user->load('tickets'));;
+        }
+
+        return new UserResource($user);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserRequest $request, $user_id)
+    {
+        // PATCH
+        try {
+            $user = User::findOrFail($user_id);
+
+            // Policy
+            $this->isAble('update', $user);
+
+            $user->update($request->mappedAttributes());
+
+            return new UserResource($user);
+        } catch (ModelNotFoundException $exception) {
+            return $this->error('User cannot be found.', 404);
+        } catch (AuthorizationException $ex) {
+            return $this->error('You are not authorized to update that resource.', 401);
+        }
+    }
+
+    public function replace(ReplaceUserRequest $request, $user_id)
+    {
+        // PUT
+        try {
+            $user = User::findOrFail($user_id);
+
+            // Policy
+            $this->isAble('replace', $user);
+
+            $user->update($request->mappedAttributes());
+
+            return new UserResource($user);
+        } catch (ModelNotFoundException $exception) {
+            return $this->error('User cannot be found.', 404);
+        } catch (AuthorizationException $ex) {
+            return $this->error('You are not authorized to update that resource.', 401);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($user_id)
+    {
+        try {
+            $user = User::findOrFail($user_id);
+
+            // Policy
+            $this->isAble('delete', $user);
+
+            $user->delete();
+
+            return $this->ok('User deleted successfully');
+        } catch (ModelNotFoundException $exception) {
+            return $this->error('User not found.', 404);
+        }
+    }
+}
